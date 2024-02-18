b09d4c369b5003d975b76c600d40e6db3a396947
Shivam <70581178+shivamkonkar@users.noreply.github.com>
2024-02-18 12:37:34 +0530

    Update #2-3code.js

--- #2-3code.js
+++ #2-3code.js
@@ -1,1 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'filename.txt');
 
+fs.readFile(filePath, 'utf8', (err, data) => {
+if (err) {
+  console.error('Error reading file:', err);
+  return;
+}
+console.log('File contents:', data);
+});
